250ea39470b76e01767e3b6622394ef7cef7ff13
Matei Nenciu <matei.nenciu@ejobsgroup.ro>
2022-07-11 18:46:38 +0300

    Piano page events ios part 1

--- ios/Plugin/PianoDmp.swift
+++ ios/Plugin/PianoDmp.swift
@@ -1,8 +1,31 @@
 import Foundation
+import Capacitor
+import CxenseSDK
 
 @objc public class PianoDmp: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+    @objc public func sendPageView(_ call: CAPPluginCall, siteId: String, location: String) -> Void {
+        do {
+            let builder = PageViewEventBuilder.makeBuilder(withName: "eJobs", siteId: siteId)
+                .setLocation(loc: location)
+            Cxense.reportEvent(try builder.build())
+            
+            call.resolve()
+        } catch {
+            let messageIntro = "Sending piano event failed."
+            switch error {
+            case IncorrectConfiguration.valueRequired(let cause):
+                call.reject("\(messageIntro) Cause: \(cause)")
+            case IncorrectConfiguration.invalidParameter(let cause, let recommendation):
+                call.reject("\(messageIntro) Cause: \(cause). Recommandation: \(recommendation)")
+            case IncorrectConfiguration.conflictingParameters(let cause, let recommendation):
+                call.reject("\(messageIntro) Cause: \(cause). Recommandation: \(recommendation)")
+            case IncorrectConfiguration.freeDiskSpace:
+                call.reject("\(messageIntro) Not enough free disk space")
+            default:
+                call.reject("\(messageIntro)")
+            }
+        }
+
+        call.resolve()
     }
 }
--- ios/Plugin/PianoDmpPlugin.swift
+++ ios/Plugin/PianoDmpPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import CxenseSDK
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -9,10 +10,31 @@ import Capacitor
 public class PianoDmpPlugin: CAPPlugin {
     private let implementation = PianoDmp()
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+    override public func load() {
+        let username = getConfigValue("username") as? String
+        let apiKey = getConfigValue("apiKey") as? String
+        let config = Configuration(withUserName: username ?? "", apiKey: apiKey ?? "")
+        
+        do {
+            try Cxense.initialize(withConfiguration: config)
+        } catch {
+            NSLog("%s", "An error occurred while trying to initialize Piano")
+        }
+    }
+    
+    @objc func sendPageView(_ call: CAPPluginCall) {
+        let siteId = getConfigValue("siteId") as? String
+        if (siteId == nil || siteId == "") {
+            call.reject("Missing required site id parameter")
+            return
+        }
+        
+        let location = call.getString("location") ?? ""
+        if (location == nil || location == "") {
+            call.reject("Missing required location parameter")
+            return
+        }
+        
+        implementation.sendPageView(call, siteId: siteId ?? "", location: location ?? "")
     }
 }
