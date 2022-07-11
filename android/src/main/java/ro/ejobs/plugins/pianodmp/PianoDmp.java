126d9f0a036c9dbcbb1b7e9bd79950e34481ad31
Matei Nenciu <matei.nenciu@ejobsgroup.ro>
2022-07-11 12:27:47 +0300

    Add various checks and move logic to different file

--- android/src/main/java/ro/ejobs/plugins/pianodmp/PianoDmp.java
+++ android/src/main/java/ro/ejobs/plugins/pianodmp/PianoDmp.java
@@ -1,10 +1,91 @@
 package ro.ejobs.plugins.pianodmp;
 
 import android.util.Log;
+import com.cxense.cxensesdk.CredentialsProvider;
+import com.cxense.cxensesdk.CxenseConfiguration;
+import com.cxense.cxensesdk.CxenseSdk;
+import com.cxense.cxensesdk.model.PageViewEvent;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginConfig;
+import java.util.Iterator;
 
 public class PianoDmp {
-    public String sendPageView(String value) {
-        Log.i("Echo", value);
-        return value;
+
+    CxenseConfiguration cxConfig = CxenseSdk.getInstance().getConfiguration();
+
+    public void sendPageView(PluginCall call, PluginConfig config) {
+        String siteId = config.getString("siteId");
+        if (siteId == null || siteId == "") {
+            call.reject("Missing required siteId param");
+            return;
+        }
+
+        String pianoUsername = config.getString("username");
+        if (pianoUsername == null || pianoUsername == "") {
+            call.reject("Missing required username param");
+            return;
+        }
+
+        String pianoApiKey = config.getString("apiKey");
+        if (pianoApiKey == null || pianoApiKey == "") {
+            call.reject("Missing required api key param");
+            return;
+        }
+
+        PageViewEvent.Builder builder = new PageViewEvent.Builder(siteId);
+        if (!call.getData().has("location")) {
+            call.reject("Missing required location parameter");
+            return;
+        }
+
+        try {
+            cxConfig.setCredentialsProvider(
+                new CredentialsProvider() {
+                    @Override
+                    public String getUsername() {
+                        return pianoUsername;
+                    }
+
+                    @Override
+                    public String getApiKey() {
+                        return pianoApiKey;
+                    }
+                }
+            );
+
+            String location = call.getString("location");
+            builder.setLocation(location);
+
+            if (call.getData().has("userParams")) {
+                JSObject userParams = call.getObject("userParams", new JSObject());
+                Iterator<String> userParamsKeys = userParams.keys();
+
+                while (userParamsKeys.hasNext()) {
+                    String userParamKey = userParamsKeys.next();
+                    if (userParams.has(userParamKey)) {
+                        builder.addCustomUserParameter(userParamKey, userParams.getString(userParamKey));
+                    }
+                }
+            }
+
+            if (call.getData().has("customParams")) {
+                JSObject customParams = call.getObject("customParams", new JSObject());
+                Iterator<String> customParamsKeys = customParams.keys();
+
+                while (customParamsKeys.hasNext()) {
+                    String customParamKey = customParamsKeys.next();
+                    if (customParams.has(customParamKey)) {
+                        builder.addCustomParameter(customParamKey, customParams.getString(customParamKey));
+                    }
+                }
+            }
+
+            CxenseSdk.getInstance().pushEvents(builder.build());
+        } catch (Exception e) {
+            Log.e("Cxense error", e.getMessage());
+            call.reject(e.getMessage(), e);
+            return;
+        }
     }
 }
--- android/src/main/java/ro/ejobs/plugins/pianodmp/PianoDmpPlugin.java
+++ android/src/main/java/ro/ejobs/plugins/pianodmp/PianoDmpPlugin.java
@@ -1,79 +1,41 @@
 package ro.ejobs.plugins.pianodmp;
 
+import android.Manifest;
 import android.util.Log;
-
 import com.cxense.cxensesdk.CredentialsProvider;
 import com.cxense.cxensesdk.CxenseConfiguration;
 import com.cxense.cxensesdk.CxenseSdk;
 import com.cxense.cxensesdk.model.PageViewEvent;
 import com.getcapacitor.JSObject;
+import com.getcapacitor.PermissionState;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-
+import com.getcapacitor.annotation.Permission;
+import com.getcapacitor.annotation.PermissionCallback;
 import java.util.Iterator;
 
-@CapacitorPlugin(name = "PianoDmp")
+@CapacitorPlugin(name = "PianoDmp", permissions = { @Permission(alias = "phoneState", strings = { Manifest.permission.READ_PHONE_STATE }) })
 public class PianoDmpPlugin extends Plugin {
 
-    // private PianoDmp implementation = new PianoDmp();
-    CxenseConfiguration config = CxenseSdk.getInstance().getConfiguration();
-    PageViewEvent.Builder builder = new PageViewEvent.Builder("1136227972865927406");
+    private PianoDmp implementation = new PianoDmp();
 
-    @PluginMethod(returnType = PluginMethod.RETURN_NONE)
+    @PluginMethod
     public void sendPageView(PluginCall call) {
-        if (!call.getData().has("location")) {
-            call.reject("Missing required location parameter");
-            return;
+        if (getPermissionState("phoneState") != PermissionState.GRANTED) {
+            requestPermissionForAlias("phoneState", call, "onPermissionGranted");
+        } else {
+            implementation.sendPageView(call, getConfig());
         }
+    }
 
-        try {
-            config.setCredentialsProvider(new CredentialsProvider() {
-                @Override
-                public String getUsername() {
-                    return "SEXY-EMAIL"; // load it from secured storage
-                }
-                @Override
-                public String getApiKey() {
-                    return "SEXY-KEY"; // load it from secured storage
-                }
-            });
-
-            String location = call.getString("location");
-            builder.setLocation(location);
-
-            if (call.getData().has("userParams")) {
-                JSObject userParams = call.getObject("userParams", new JSObject());
-                Iterator<String> userParamsKeys = userParams.keys();
-
-                while (userParamsKeys.hasNext()) {
-                    String userParamKey = userParamsKeys.next();
-                    if (userParams.has(userParamKey)) {
-                        builder.addCustomUserParameter(userParamKey, userParams.getString(userParamKey));
-                    }
-                }
-            }
-
-            if (call.getData().has("customParams")) {
-                JSObject customParams = call.getObject("customParams", new JSObject());
-                Iterator<String> customParamsKeys = customParams.keys();
-
-                while (customParamsKeys.hasNext()) {
-                    String customParamKey = customParamsKeys.next();
-                    if (customParams.has(customParamKey)) {
-                        builder.addCustomUserParameter(customParamKey, customParams.getString(customParamKey));
-                    }
-                }
-            }
-
-            CxenseSdk.getInstance().pushEvents(builder.build());
-        } catch (Exception e) {
-            Log.e("Cxense error", e.getMessage());
-            call.reject(e.getMessage(), e);
-            return;
+    @PermissionCallback
+    private void onPermissionGranted(PluginCall call) {
+        if (getPermissionState("phoneState") == PermissionState.GRANTED) {
+            implementation.sendPageView(call, getConfig());
+        } else {
+            call.reject("Permissions required for making Piano API calls");
         }
-
-        call.resolve();
     }
 }
